069636953a5d7409e548b929040df6137d94cc8b
Asaf Fadida <asaf@neokarm.com>
2020-05-21 12:07:58 +0300

    Changed package version to be git hash

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
+import os
+import subprocess
 from setuptools import setup, find_packages
 from zadarapy import __version__
 
+def get_git_revision():
+    """Return the git revision."""
+    if os.path.exists('PKG-INFO'):
+        with open('PKG-INFO') as package_info:
+            for key, value in (line.split(':', 1) for line in package_info):
+                if key.startswith('Version'):
+                    return value.strip()
+
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
+
 setup(
     name='zadarapy',
-    version=__version__,
+    version=get_git_revision(),
     packages=find_packages(),
     install_requires=['configparser>=3.5.0', 'future>=0.15.2',
                       'terminaltables>=2.1.0', 'requests>=2.2.1'],
